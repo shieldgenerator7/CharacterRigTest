3d1872fe9b1b9a310bdeef3e5b406eb7dd362cbf
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-07-25 00:06:29 -0400

    Improve firefly movement

--- Assets/Scripts/FireflyController.cs
+++ Assets/Scripts/FireflyController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FireflyController : MonoBehaviour
 {
     public float moveSpeed = 1;
+    public float dirChangeDelay = 0.2f;
+
+    private float lastDirChangeTime = 0;
 
     private Rigidbody2D rb2d;
 
@@ -17,7 +20,11 @@ public class FireflyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveDir = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
-        rb2d.velocity = moveDir.normalized * moveSpeed;
+        if (Time.time >= lastDirChangeTime + dirChangeDelay)
+        {
+            lastDirChangeTime = Time.time;
+            Vector2 moveDir = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+            rb2d.velocity = moveDir.normalized * moveSpeed;
+        }
     }
 }
