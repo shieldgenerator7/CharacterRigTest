8b821128507dc937ede7eac3869cf10500682dbd
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-08-12 04:06:17 -0400

    Hookup anim: Piper anthro walk run

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -24,8 +24,11 @@ public class PlayerAnimator : MonoBehaviour
     void UpdateAnimator(PlayerState playerState)
     {
         bool feral = playerState.form == PlayerState.Form.FERAL;
+        bool moving = playerState.moveDirection != 0;
         //Anthro/Feral form
         animator.SetBool("feral", feral);
+        animator.SetBool("walking", moving && !playerState.running);
+        animator.SetBool("running", moving && playerState.running);
         //Flip X
         float lookDirection = (feral) ? playerState.moveDirection : playerState.lookDirection.x;
         if (lookDirection != 0)
